790b079c665e7a25425dfe4dffa5cb2d0b9bf7a0
Level-Up-4 <vikash-verma-profile>
2022-07-26 11:27:39 +0530

    first commit

---
+++ filehandling.py
@@ -0,0 +1,20 @@
+
+
+#f1=open("sample.txt","a")
+# f1=open("sample.txt","w")
+# f1.write("Suresh Kumar");
+# f1.close();
+
+# f=open("sample.txt","r")
+# print(f.read())
+# #print(f.read(3))
+# f.close();
+
+# f=open("myfile1.txt","x")
+# f.close()
+# f=open("myfile2.txt","w")
+# f.close()
+import os
+os.remove("myfile1.txt");
+
+os.rmdir("");
