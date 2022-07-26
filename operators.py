14c90d2632d0587f4e688a66d7616a2da810fbf8
Level-Up-4 <vikash-verma-profile>
2022-07-26 10:24:13 +0530

    first commit

---
+++ operators.py
@@ -0,0 +1,14 @@
+s=2+3
+print(s)
+b=7
+a=9
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+a=34
+b=34
+if(b>a):
+      print("b is greater than a")
+elif a==b:
+    print("a is equal to b")
