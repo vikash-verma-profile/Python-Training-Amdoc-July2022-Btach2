4b15b5118d5db57b2e3bfcfbaf992daad8f305e4
Level-Up-4 <vikash-verma-profile>
2022-07-26 12:41:54 +0530

    first commit

---
+++ Lists.py
@@ -0,0 +1,32 @@
+mylist=["vikash 1","Vikash 2"]
+print(mylist)
+print(mylist[0])
+
+#Ordered
+#changable
+#allow duplicate values
+
+#Tuples
+#Ordered
+#unchangable
+mytuple=("appkle","banana","orange")
+print(mytuple[0])
+print(mytuple)
+
+#Sets
+#unordred,unchagable,and unindexed
+
+thisset={"apple","banana","orange"}
+print(thisset);
+
+for x in thisset:
+    print(x)
+
+
+thisdist={
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+print("============================")
+print(thisdist['brand'])
